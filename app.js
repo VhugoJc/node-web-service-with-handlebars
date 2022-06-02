605bc3fac7120132af9cd4e3d9d1b685d2fa06f5
VhugoJC <vic_360.2000@hotmail.com>
2022-06-01 22:04:31 -0500

    Web Service

---
+++ app.js
@@ -0,0 +1,60 @@
+
+const express = require('express');
+const hbs = require('hbs');
+
+const app = express();
+const port = 3000;
+
+
+// handlebar para cuando no se usa react ni nada hacerlo semi dinamico y reutilizar codigo
+//handlebart utiliza vmc (Vista modelo compilador)
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+/*
+    NOTA: Los headersa son los componentes para reutilizar desde el backeend
+*/
+
+
+// Middleware static content
+app.use(express.static('public'));
+
+
+
+//renderizar paginas para usar hbs:
+
+//controlador:
+app.get('/',(req,res)=>{
+    res.render('home',{
+        //argumento
+        name: 'Hugo',
+        title: 'curso node'
+    });
+})
+app.get('/generic',(req,res)=>{
+    res.render('generic',{
+        //argumento
+        name: 'Hugo',
+        title: 'curso node'
+    });
+})
+app.get('/elements',(req,res)=>{
+    res.render('elements',{
+        //argumento
+        name: 'Hugo',
+        title: 'curso node'
+    });
+})
+
+
+// app.get('/generic', function(req, res) { //use generic without .html
+//     res.sendFile(__dirname+'/public/generic.html');
+// });
+// app.get('/elements', function(req, res) { //use generic without .html
+//     res.sendFile(__dirname+'/public/elements.html');
+// });
+
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+  })
+  
